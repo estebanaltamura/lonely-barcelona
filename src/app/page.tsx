44ea198a9721875aea559d211bb650e8217e85fb
Andres Altamura <esteban.altamura@gmail.com>
2024-06-18 21:24:12 +0200

    furst commit

---
+++ src/app/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Box, Typography, useMediaQuery } from '@mui/material';
+
+const Home = () => {
+  const isDesktop = useMediaQuery('(min-width:600px)');
+
+  return (
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Box sx={{ display: 'flex', width: '287px', height: '298px' }}>
+        <img
+          src='/logo.png'
+          alt=''
+          style={{ width: '287px', height: '298px' }}
+        />
+      </Box>
+
+      <Typography
+        variant='body1'
+        className='newsreader'
+        sx={{
+          color: '#FDF0DE',
+          fontSize: isDesktop ? '64px' : '24px',
+          fontStyle: 'italic',
+          textAlign: 'center',
+          marginTop: '20px',
+        }}
+      >
+        We’re cocktails, music, and groove.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Home;
