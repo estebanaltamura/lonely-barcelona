d378f732ab850a35e4e49a2f2c1d69c7c39c320f
Andres Altamura <esteban.altamura@gmail.com>
2024-06-23 13:42:28 +0200

    Update page.tsx

--- src/app/carta/page.tsx
+++ src/app/carta/page.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const Carta: React.FC = () => {
   return (
     <Box sx={{ width: '100vw', height: '100%' }}>
-      <img src='/carta-web-final.jpg' alt='' style={{ width: '100%' }} />
+      <img src='/carta-web-final.png' alt='' style={{ width: '100%' }} />
     </Box>
   );
 };
