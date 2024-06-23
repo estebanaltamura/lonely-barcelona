364de36d5fdaf57ace40d709be50089e6a15bdc6
lonely.bar.barcelona@gmail.com <lonely.bar.barcelona@gmail.com>
2024-06-23 18:16:36 +0200

    update

--- src/app/carta/page.tsx
+++ src/app/carta/page.tsx
@@ -4,9 +4,96 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 const Carta: React.FC = () => {
+  const cocktails = [
+    {
+      titulo: 'Carrotcake garibaldi',
+      descripcion:
+        'cscscscs cscscscs cscscscs cscscscs cscscscs cscscscs cscscscs cscscscs cscscscscscscscs cscscscs cscscscs cscscscs cscscscs',
+      precio: 11,
+    },
+  ];
+
+  const vinos = [
+    {
+      titulo: 'Cocktails de autor',
+      descripcion: 'Esto es una descripcion',
+      precio: 11,
+    },
+  ];
+
+  const cervezas = [
+    {
+      titulo: 'Cocktails de autor',
+      descripcion: 'Esto es una descripcion',
+      precio: 11,
+    },
+  ];
+
   return (
-    <Box sx={{ width: '100vw', height: '100%' }}>
-      <img src='/carta-web-final.png' alt='' style={{ width: '100%' }} />
+    <Box
+      className='newsReaderFont'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        padding: '0 13px',
+        color: '#FCF0DD',
+      }}
+    >
+      {/* Título de sección */}
+      <Box sx={{ width: '300px', color: 'white', fontSize: '41px' }}>
+        Cocktails de autor
+      </Box>
+
+      {/* Items container*/}
+      <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
+        {cocktails.map((item, index) => (
+          <Box key={index} sx={{ marginBottom: '10px' }}>
+            {/* header */}
+            <Box sx={{ display: 'flex', width: '100%' }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  width: '80px',
+                  flexWrap: 'wrap',
+                  fontSize: '25px',
+                }}
+              >
+                {item.titulo}
+              </Box>
+              <Box sx={{ display: 'flex', flexGrow: 1 }}></Box>
+              <Box
+                sx={{ display: 'flex', alignItems: 'end' }}
+              >{`${item.precio} €`}</Box>
+            </Box>
+            {/*Description */}
+            <Box
+              sx={{
+                position: 'relative',
+                lineHeight: '18px',
+                fontSize: '14px',
+                marginTop: '7px',
+                paddingLeft: '8px',
+              }}
+            >
+              <Box
+                sx={{
+                  '::before': {
+                    content: '""',
+                    position: 'absolute',
+                    left: 0,
+                    top: '10%',
+                    bottom: '10%',
+                    width: '1px',
+                    backgroundColor: '#FCF0DD',
+                  },
+                }}
+              ></Box>
+              {item.descripcion}
+            </Box>
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 };
