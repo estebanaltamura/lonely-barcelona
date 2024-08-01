7ef4fe08c727c06577d1aa53df1f48ab748177e5
Andres <esteban.altamura@gmail.com>
2024-08-01 14:11:40 +0200

    update

--- src/app/cartas/carta-espanol/page.tsx
+++ src/app/cartas/carta-espanol/page.tsx
@@ -337,6 +337,89 @@ const Carta: React.FC = () => {
           </Box>
         ))}
       </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+          margin: '40px 0 20px 0',
+        }}
+      >
+        <Divider
+          sx={{ width: '100%', height: '1.5px', backgroundColor: '#FCF0DD' }}
+        />
+      </Box>
+
+      {/* Título de sección */}
+      <Box
+        sx={{
+          width: '240px',
+          fontSize: '55px',
+          lineHeight: '29px',
+          fontFamily: 'Happy Time Three',
+          color: '#FCF0DD',
+          marginTop: '45px',
+        }}
+      >
+        Comidas
+      </Box>
+
+      {/* Items container*/}
+      <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '55px' }}>
+        {carta.comida.map((item, index) => (
+          <Box key={index} sx={{ marginBottom: '35px' }}>
+            {/* header */}
+            <Box sx={{ display: 'flex', width: '100%', fontSize: '32px' }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  width: '160px',
+                  lineHeight: '25px',
+                  flexWrap: 'wrap',
+                  fontFamily: 'Happy Time Three',
+                }}
+              >
+                {item.title}
+              </Box>
+              <Box sx={{ display: 'flex', flexGrow: 1 }}></Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'end',
+                  fontFamily: 'Happy Time Three',
+                  lineHeight: '25px',
+                }}
+              >
+                {item.price} €<span>&nbsp;</span>
+              </Box>
+            </Box>
+            {/*Description */}
+            <Box
+              sx={{
+                position: 'relative',
+
+                marginTop: '13px',
+                paddingLeft: '8px',
+              }}
+            >
+              <Box
+                sx={{
+                  '::before': {
+                    content: '""',
+                    position: 'absolute',
+                    left: 0,
+                    top: '4%',
+                    bottom: '2%',
+                    width: '1px',
+                    backgroundColor: '#FCF0DD',
+                  },
+                }}
+              ></Box>
+            </Box>
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 };
