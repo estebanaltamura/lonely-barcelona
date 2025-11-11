8166a8216d6a928725e455d2251aa65f776eca73
estenbanaltamura <estenban.altamura@gmail.com>
2025-11-11 15:28:19 +0100

    a chequear

--- src/app/cartas/carta-catala/page.tsx
+++ src/app/cartas/carta-catala/page.tsx
@@ -676,6 +676,301 @@ const CartaCatala: React.FC = () => {
         ))}
       </Box>
 
+
+<Box
+                sx={{
+                  display: "flex",
+                  width: "100%",
+                  justifyContent: "center",
+                  margin: "40px 0 20px 0",
+                }}
+              >
+                <Divider
+                  sx={{ width: "100%", height: "1.5px", backgroundColor: "#FCF0DD" }}
+                />
+              </Box>
+        
+              {/* Título de sección */}
+              <Box
+                sx={{
+                  width: "240px",
+                  fontSize: "55px",
+                  lineHeight: "29px",
+                  fontFamily: "Happy Time Three",
+                  color: "#FCF0DD",
+                  marginTop: "45px",
+                }}
+              >
+                Rones
+              </Box>
+        
+              {/* Items container*/}
+              <Box sx={{ display: "flex", flexDirection: "column", marginTop: "55px" }}>
+                {carta.rones.map((item, index) => (
+                  <Box key={index} sx={{ marginBottom: "35px" }}>
+                    {/* header */}
+                    <Box sx={{ display: "flex", width: "100%", fontSize: "32px" }}>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          width: "160px",
+                          lineHeight: "25px",
+                          flexWrap: "wrap",
+                          fontFamily: "Happy Time Three",
+                        }}
+                      >
+                        {item.title}
+                      </Box>
+                      <Box sx={{ display: "flex", flexGrow: 1 }}></Box>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "end",
+                          fontFamily: "Happy Time Three",
+                          lineHeight: "25px",
+                        }}
+                      >
+                        {item.price} €<span>&nbsp;</span>
+                      </Box>
+                    </Box>
+                    {/*Description */}
+                    <Box
+                      sx={{
+                        position: "relative",
+        
+                        marginTop: "13px",
+                        paddingLeft: "8px",
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          "::before": {
+                            content: '""',
+                            position: "absolute",
+                            left: 0,
+                            top: "4%",
+                            bottom: "2%",
+                            width: "1px",
+                            backgroundColor: "#FCF0DD",
+                          },
+                        }}
+                      ></Box> 
+                      
+                      
+                      <Typography
+                        sx={{ fontSize: "12px", marginTop: "11px", lineHeight: "13px" }}
+                        className="majorMonoDisplayFont"
+                      >{`·alergenos: ${item.alergenos}`}</Typography>
+                    </Box>
+                  </Box>
+                ))}
+              </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+              <Box
+                      sx={{
+                        display: "flex",
+                        width: "100%",
+                        justifyContent: "center",
+                        margin: "40px 0 20px 0",
+                      }}
+                    >
+                      <Divider
+                        sx={{ width: "100%", height: "1.5px", backgroundColor: "#FCF0DD" }}
+                      />
+                    </Box>
+              
+                    {/* Título de sección */}
+                    <Box
+                      sx={{
+                        width: "240px",
+                        fontSize: "55px",
+                        lineHeight: "29px",
+                        fontFamily: "Happy Time Three",
+                        color: "#FCF0DD",
+                        marginTop: "45px",
+                      }}
+                    >
+                      Agaves
+                    </Box>
+              
+                    {/* Items container*/}
+                    <Box sx={{ display: "flex", flexDirection: "column", marginTop: "55px" }}>
+                      {carta.agaves.map((item, index) => (
+                        <Box key={index} sx={{ marginBottom: "35px" }}>
+                          {/* header */}
+                          <Box sx={{ display: "flex", width: "100%", fontSize: "32px" }}>
+                            <Box
+                              sx={{
+                                display: "flex",
+                                width: "160px",
+                                lineHeight: "25px",
+                                flexWrap: "wrap",
+                                fontFamily: "Happy Time Three",
+                              }}
+                            >
+                              {item.title}
+                            </Box>
+                            <Box sx={{ display: "flex", flexGrow: 1 }}></Box>
+                            <Box
+                              sx={{
+                                display: "flex",
+                                alignItems: "end",
+                                fontFamily: "Happy Time Three",
+                                lineHeight: "25px",
+                              }}
+                            >
+                              {item.price} €<span>&nbsp;</span>
+                            </Box>
+                          </Box>
+                          {/*Description */}
+                          <Box
+                            sx={{
+                              position: "relative",
+              
+                              marginTop: "13px",
+                              paddingLeft: "8px",
+                            }}
+                          >
+                            <Box
+                              sx={{
+                                "::before": {
+                                  content: '""',
+                                  position: "absolute",
+                                  left: 0,
+                                  top: "4%",
+                                  bottom: "2%",
+                                  width: "1px",
+                                  backgroundColor: "#FCF0DD",
+                                },
+                              }}
+                            ></Box>
+                  <Typography
+                                    sx={{ fontSize: "12px", lineHeight: "13px", marginTop: "19px" }}
+                                    className="majorMonoDisplayFont"
+                                  >
+                                    {item.description}
+                                  </Typography>
+                           
+                            <Typography
+                              sx={{ fontSize: "12px", marginTop: "11px", lineHeight: "13px" }}
+                              className="majorMonoDisplayFont"
+                            >{`·alergenos: ${item.alergenos}`}</Typography>
+                          </Box>
+                        </Box>
+                      ))}
+                    </Box>
+
+
+
+
+
+
+
+
+                    <Box
+                            sx={{
+                              display: "flex",
+                              width: "100%",
+                              justifyContent: "center",
+                              margin: "40px 0 20px 0",
+                            }}
+                          >
+                            <Divider
+                              sx={{ width: "100%", height: "1.5px", backgroundColor: "#FCF0DD" }}
+                            />
+                          </Box>
+                    
+                          {/* Título de sección */}
+                          <Box
+                            sx={{
+                              width: "240px",
+                              fontSize: "55px",
+                              lineHeight: "29px",
+                              fontFamily: "Happy Time Three",
+                              color: "#FCF0DD",
+                              marginTop: "45px",
+                            }}
+                          >
+                            Vodkas
+                          </Box>
+                    
+                          {/* Items container*/}
+                          <Box sx={{ display: "flex", flexDirection: "column", marginTop: "55px" }}>
+                            {carta.vodkas.map((item, index) => (
+                              <Box key={index} sx={{ marginBottom: "35px" }}>
+                                {/* header */}
+                                <Box sx={{ display: "flex", width: "100%", fontSize: "32px" }}>
+                                  <Box
+                                    sx={{
+                                      display: "flex",
+                                      width: "160px",
+                                      lineHeight: "25px",
+                                      flexWrap: "wrap",
+                                      fontFamily: "Happy Time Three",
+                                    }}
+                                  >
+                                    {item.title}
+                                  </Box>
+                                  <Box sx={{ display: "flex", flexGrow: 1 }}></Box>
+                                  <Box
+                                    sx={{
+                                      display: "flex",
+                                      alignItems: "end",
+                                      fontFamily: "Happy Time Three",
+                                      lineHeight: "25px",
+                                    }}
+                                  >
+                                    {item.price} €<span>&nbsp;</span>
+                                  </Box>
+                                </Box>
+                                {/*Description */}
+                                <Box
+                                  sx={{
+                                    position: "relative",
+                    
+                                    marginTop: "13px",
+                                    paddingLeft: "8px",
+                                  }}
+                                >
+                                  <Box
+                                    sx={{
+                                      "::before": {
+                                        content: '""',
+                                        position: "absolute",
+                                        left: 0,
+                                        top: "4%",
+                                        bottom: "2%",
+                                        width: "1px",
+                                        backgroundColor: "#FCF0DD",
+                                      },
+                                    }}
+                                  ></Box>
+                    
+                              
+                                  <Typography
+                                    sx={{ fontSize: "12px", marginTop: "11px", lineHeight: "13px" }}
+                                    className="majorMonoDisplayFont"
+                                  >{`·alergenos: ${item.alergenos}`}</Typography>
+                                </Box>
+                              </Box>
+                            ))}
+                          </Box>
+
+
+
+      
+
       <Box
         sx={{
           display: "flex",
