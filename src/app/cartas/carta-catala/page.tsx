3f6d6eab27912903b8ced472e7901c67f90a2f2d
Andres Altamura <esteban.altamura@gmail.com>
2025-12-12 17:04:11 +0100

    Update page.tsx

--- src/app/cartas/carta-catala/page.tsx
+++ src/app/cartas/carta-catala/page.tsx
@@ -203,7 +203,7 @@ verticalAlign: 'sub',
   >
     {item.description}
 
-    <span sx={{ position: "relative", top:"3px" }}>
+    <span style={{ position: "relative", top:"3px" }}>
       <LinkIcon sx={{ fontSize: 13 }} />
     </span>
   </Typography>
