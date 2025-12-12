ce1f52a256e1c93ebbc28d0c604637c550f08656
Andres Altamura <esteban.altamura@gmail.com>
2025-12-12 15:59:41 +0100

    Change 'allergens' to 'alergenos' in Typography

--- src/app/cartas/carta-ingles/page.tsx
+++ src/app/cartas/carta-ingles/page.tsx
@@ -1103,7 +1103,7 @@ const CartaCatala: React.FC = () => {
               <Typography
                 sx={{ fontSize: "12px", marginTop: "11px", lineHeight: "13px" }}
                 className="majorMonoDisplayFont"
-              >{`·allergens: ${item.allergens}`}</Typography>
+              >{`·allergens: ${item.alergenos}`}</Typography>
             </Box>
           </Box>
         ))}
